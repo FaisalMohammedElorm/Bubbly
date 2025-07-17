8bd47f67b55e533b2607668c14338f499df1cf7d
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-07-17 22:00:08 +0000

    Splash Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from 'react-native'
+import React from 'react'
+import { colors } from '@/constants/theme';
+import Animated, { FadeInDown } from 'react-native-reanimated'
 
-export default function Index() {
+const SplashScreen = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle={'light-content'} backgroundColor={colors.neutral900} />
+      <Animated.Image 
+        source={require('@/assets/images/splashImage.png')}
+        entering={FadeInDown.duration(700).springify()}
+        style={styles.logo}
+        resizeMode={"contain"}
+      />
     </View>
-  );
+  )
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.neutral900
+  },
+  logo:{
+    height: '23%',
+    aspectRatio: 1,
+  }
+  
+});
+export default SplashScreen
